559b1980e39cca13f183eaca99ef9ef5f458a66c
Peter <peterjbark@gmail.com>
2023-06-08 17:48:41 -0700

    created Person class

--- lab.js
+++ lab.js
@@ -1,4 +1,4 @@
-class Hamster{
+class Hamster {
     constructor(name){
         this.name = name;
         this.owner = "";
@@ -11,6 +11,55 @@ class Hamster{
         console.log("nibble nibble")
     }
     getPrice(){
-        return price
+    return (`${this.price}`)
     }
 }
+const hamster1 = new Hamster("Jerry");
+
+class Person {
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log("Hi! I'm " + `${this.name}` + ", nice to meet you.")
+    }
+    eat(){
+        this.weight += 5;
+        this.mood += 3;
+        console.log("This is some good food.")
+    }
+    exercise(){
+        this.weight -= 2;
+        console.log("Working out sucks.")
+    }
+    ageUp(){
+        this.age += 10
+        this.height += 10
+        this.weight += 10
+        this.mood -= 10
+        this.bankAccount += 10
+    }
+    buyHamster(hamster){
+        this.hamsters.push(Hamster)
+        this.mood += 10
+        this.bankAccount -= this.price;
+    }
+}
+const person1 = new Person("Terry")
+person1.buyHamster("bob");
+console.log(person1);
